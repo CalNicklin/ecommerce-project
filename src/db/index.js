5e156d927183418ab06bbba6cb2c8dbf37382be0
Cal <calpnicklin@gmail.com>
2022-12-10 06:53:40 +0000

    adds gitignore

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DB,
+    password: process.env.PASSWORD,
+    port: process.env.DBPORT,
+})
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
