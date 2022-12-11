510d3b7fd359b2a876ee896ecf6504c8a096f96a
Cal <calpnicklin@gmail.com>
2022-12-11 11:06:25 +0000

    add various endpoints

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 
 const pool = new Pool({
     user: process.env.USER,
@@ -6,8 +6,102 @@ const pool = new Pool({
     database: process.env.DB,
     password: process.env.PASSWORD,
     port: process.env.DBPORT,
-})
+});
+
+// User Endpoints \\
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    });
+};
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    });
+};
+
+const createUser = (request, response) => {
+    const { name, email, password } = request.body;
+
+    pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, password], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    });
+};
+
+const updateUser = (request, response) => {
+    const id = parseInt(request.params.id);
+    const { name, email, password } = request.body;
+
+    pool.query(
+        'UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4',
+        [name, email, password, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User modified with ID:${id}`)
+        }
+    );
+};
+
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query(
+        'DELETE FROM users WHERE id = $1',
+        [id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User deleted with ID:${id}`)
+        }
+    );
+};
+
+// End user endpoints \\
+// Products endoints \\
+
+const getProducts = (request, response) => {
+    pool.query('SELECT * FROM products ORDER BY product_name ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    });
+};
+
+const getProductsBySku = (request, response) => {
+    const sku = request.params.sku;
+
+    pool.query('SELECT * FROM products WHERE sku = $1', [sku], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    });
+};
+
+
 
 module.exports = {
-    query: (text, params) => pool.query(text, params),
-}
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    getProducts,
+    getProductsBySku
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 const bodyParser = require('body-parser');
+const db = require('./db');
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,33 @@ app.use(
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// Endpoints:
+// /users
+app.get('/users', db.getUsers);
+
+// register
+app.post('/register', db.createUser);
+// curl -d '{"name":"Chloe N", "email":"chlo@cal.com", "password":"passw0rd"}' -H "Content-Type: application/json" -X POST http://localhost:3000/users/1
+
+// /users/:id
+app.get('/users/:id', db.getUserById);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+// /login
+
+// /products
+app.get('/products', db.getProducts);
+
+// /products/:sku
+app.get('/products/:sku', db.getProductsBySku);
+
+// /cart
+// /cart/:id
+// /orders
+// /orders/:id
+
 
 app.listen(process.env.PORT, () =>
     console.log(`Listening on port ${process.env.PORT}!`),
